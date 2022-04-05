bdff5016d5018a6f7a6fadc3792c0721466a83b9
ksw-1024 <orimun.mohumohu@gmail.com>
2022-04-05 23:40:14 +0900

    liveproxyを追加

--- addon.py
+++ addon.py
@@ -1,16 +1,14 @@
 import sys
-import json
-import subprocess
 import xbmc
 import xbmcgui
 import xbmcplugin
-from resources.streamlink import Streamlink
+import streamlink
 
 addon_handle = int(sys.argv[1])
 
 xbmcplugin.setContent(addon_handle, 'movies')
 
-streams = Streamlink.streams("https://abema.tv/now-on-air/abema-news")
-url = streams['best'].to_url()
+streams = streamlink.streams("https://abema.tv/now-on-air/abema-news")
+url = "http://127.0.0.1:53422/base64/c3RyZWFtbGluayBodHRwczovL2FiZW1hLnR2L25vdy1vbi1haXIvc3BlY2lhbC1wbHVzIGJlc3QK"
 
 xbmc.Player().play(url)
