fb254d93097efa6e98500994d1e9ca3d04ae7bb2
ksw-1024 <orimun.mohumohu@gmail.com>
2022-04-06 00:42:47 +0900

    Fixed

--- addon.py
+++ addon.py
@@ -8,7 +8,6 @@ addon_handle = int(sys.argv[1])
 
 xbmcplugin.setContent(addon_handle, 'movies')
 
-streams = streamlink.streams("https://abema.tv/now-on-air/abema-news")
-url = "http://127.0.0.1:53422/base64/c3RyZWFtbGluayBodHRwczovL2FiZW1hLnR2L25vdy1vbi1haXIvc3BlY2lhbC1wbHVzIGJlc3QK"
+url = "http://127.0.0.1:53422/base64/c3RyZWFtbGluayBodHRwczovL2FiZW1hLnR2L25vdy1vbi1haXIvYWJlbWEtbmV3cyA3MjBwCg=="
 
 xbmc.Player().play(url)
--- project.test/main.py
+++
@@ -1,4 +0,0 @@
-import streamlink
-
-streams = streamlink.streams("https://abema.tv/now-on-air/abema-news")
-print(streams)
