e93e09dcf5f03558d63f8d57277ea791fedda73d
ksw-1024 <orimun.mohumohu@gmail.com>
2022-04-05 22:48:28 +0900

    Streamlinkの調整

--- addon.py
+++ addon.py
@@ -4,13 +4,13 @@ import subprocess
 import xbmc
 import xbmcgui
 import xbmcplugin
-import streamlink
+from resources.streamlink import Streamlink
 
 addon_handle = int(sys.argv[1])
 
 xbmcplugin.setContent(addon_handle, 'movies')
 
-streams = streamlink.streams("https://abema.tv/now-on-air/abema-news")
+streams = Streamlink.streams("https://abema.tv/now-on-air/abema-news")
 url = streams['best'].to_url()
 
 xbmc.Player().play(url)
