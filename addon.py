edbe56b8ebf462f2d7764b43103aee17bcb8232b
ksw-1024 <orimun.mohumohu@gmail.com>
2022-04-06 21:18:11 +0900

    addon.pyの更新

--- addon.py
+++ addon.py
@@ -2,12 +2,12 @@ import sys
 import xbmc
 import xbmcgui
 import xbmcplugin
-import streamlink
+from resources import streamlink
 
 addon_handle = int(sys.argv[1])
 
 xbmcplugin.setContent(addon_handle, 'movies')
 
-url = "http://127.0.0.1:53422/base64/c3RyZWFtbGluayBodHRwczovL2FiZW1hLnR2L25vdy1vbi1haXIvYWJlbWEtbmV3cyA3MjBwCg=="
+url = "rtsp://127.0.0.1:8554"
 
 xbmc.Player().play(url)
---
+++ project.test/main.py
@@ -0,0 +1,5 @@
+from resources import streamlink
+
+abema_news = "https://abema.tv/now-on-air/abema-news"
+
+streams_news = streamlink.streams(abema_news)
