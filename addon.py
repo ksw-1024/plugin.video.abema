56e5c5fd0536e884aef089ed3367f4b80802a023
ksw-1024 <orimun.mohumohu@gmail.com>
2022-04-09 08:54:34 +0900

    main.pyの更新

--- addon.py
+++ addon.py
@@ -3,11 +3,41 @@ import xbmc
 import xbmcgui
 import xbmcplugin
 from resources import streamlink
+# import vlc
+import socket
+import time
 
 addon_handle = int(sys.argv[1])
 
 xbmcplugin.setContent(addon_handle, 'movies')
 
-url = "rtsp://127.0.0.1:8553"
+#リンクの設定
+abema_news = "rtsp://127.0.0.1:8553"
 
-xbmc.Player().play(url)
+#VLCの設定
+# player = vlc.MediaPlayer()
+# player.set_mrl(abema_news)
+already_play = False
+loop = True
+
+#TCPポートテスト
+host = '127.0.0.1'
+port = 8553
+timeout_seconds = 1
+
+while loop == 1:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(timeout_seconds)
+    result = sock.connect_ex((host,int(port)))
+    if result == 0:
+        print("Host: {}, Port: {} - True".format(host, port))
+        if already_play == False:
+            xbmc.Player().play(abema_news)
+            already_play = True
+    else:
+        print("Host: {}, Port: {} - False".format(host, port))
+        if already_play == True:
+            xbmc.Player().stop()
+            already_play = False
+    
+    time.sleep(timeout_seconds)
--- project.test/main.py
+++ project.test/main.py
@@ -1,5 +1,35 @@
 from resources import streamlink
+import vlc
+import socket
+import time
 
-abema_news = "https://abema.tv/now-on-air/abema-news"
+#リンクの設定
+abema_news = "rtsp://127.0.0.1:8554"
 
-streams_news = streamlink.streams(abema_news)
+#VLCの設定
+player = vlc.MediaPlayer()
+player.set_mrl(abema_news)
+already_play = False
+loop = True
+
+#TCPポートテスト
+host = '127.0.0.1'
+port = 8553
+timeout_seconds = 1
+
+while loop == 1:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(timeout_seconds)
+    result = sock.connect_ex((host,int(port)))
+    if result == 0:
+        print("Host: {}, Port: {} - True".format(host, port))
+        if already_play == False:
+            player.play()
+            already_play == True
+    else:
+        print("Host: {}, Port: {} - False".format(host, port))
+        if already_play == True:
+            player.stop()
+            already_play == False
+    
+    time.sleep(timeout_seconds)
